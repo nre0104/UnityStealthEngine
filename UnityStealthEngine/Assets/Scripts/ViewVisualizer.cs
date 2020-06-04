2f7909af03e50141401bec4732db7b24530d848e
Nicolas Resch <nresch@stud.hs-heilbronn.de>
2020-06-04 18:15:13 +0200

    Added basic view visualization

--- UnityStealthEngine/Assets/Scripts/ViewVisualizer.cs
+++ UnityStealthEngine/Assets/Scripts/ViewVisualizer.cs
@@ -14,9 +14,24 @@ public class ViewVisualizer : MonoBehaviour
     [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
+    public bool visualizeViewInScene = true;
+    public float MeshResolution;
+
+    public MeshFilter ViewMeshFilter;
+    private Mesh viewMesh;
+
     void Start()
     {
         StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    void LateUpdate()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        ViewMeshFilter.mesh = viewMesh;
+
+        DrawFieldOfView();
     }
 
     IEnumerator FindTargetsWithDelay(float delay)
@@ -50,6 +65,61 @@ public class ViewVisualizer : MonoBehaviour
         }
     }
 
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(ViewAngle * MeshResolution);
+        float stepAngleSize = ViewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - ViewAngle/2 + stepAngleSize * i;
+
+            if (visualizeViewInScene)
+            {
+                ViewCastInfo newViewCastInfo = ViewCast(angle);
+                viewPoints.Add(newViewCastInfo.endPoint);
+            }
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] verticies = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount-2) * 3];
+
+        verticies[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount-1; i++)
+        {
+            verticies[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount-2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = verticies;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, dir, out hit, ViewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * ViewRadius, ViewRadius, globalAngle);
+        }
+    }
+
     public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         if (!angleIsGlobal)
@@ -57,5 +127,21 @@ public class ViewVisualizer : MonoBehaviour
             angleInDegrees += transform.eulerAngles.y;
         }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 endPoint;
+        public float dist;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _endPoint, float _dist, float _angle)
+        {
+            hit = _hit;
+            endPoint = _endPoint;
+            dist = _dist;
+            angle = _angle;
+        }
     }
 }
