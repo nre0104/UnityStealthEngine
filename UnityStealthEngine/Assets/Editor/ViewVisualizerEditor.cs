f5cc9adb64de3cd647fdff0d79ef44ad6ba6b76f
Nicolas Resch <nresch@stud.hs-heilbronn.de>
2020-06-04 16:48:00 +0200

    Visualized view in editor + changed camera distance

---
+++ UnityStealthEngine/Assets/Editor/ViewVisualizerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ViewVisualizer))]
+public class ViewVisualizerEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        ViewVisualizer viewVisualizer = (ViewVisualizer)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(viewVisualizer.transform.position, Vector3.up, Vector3.forward, 360, viewVisualizer.ViewRadius);
+
+        Vector3 viewAngleA = viewVisualizer.DirFromAngle(-viewVisualizer.ViewAngle/2, false);
+        Vector3 viewAngleB = viewVisualizer.DirFromAngle(viewVisualizer.ViewAngle/2, false);
+
+        Handles.DrawLine(viewVisualizer.transform.position, viewVisualizer.transform.position + viewAngleA * viewVisualizer.ViewRadius);
+        Handles.DrawLine(viewVisualizer.transform.position, viewVisualizer.transform.position + viewAngleB * viewVisualizer.ViewRadius);
+    }
+}
--- UnityStealthEngine/Assets/Scripts/ViewVisualizer.cs
+++ UnityStealthEngine/Assets/Scripts/ViewVisualizer.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class ViewVisualizer : MonoBehaviour
 {
-    public float ViewAngle;
     public float ViewRadius;
+    [Range(0, 360)]
+    public float ViewAngle;
 
-    public Vector3 DirFromAngle(float angleInDegrees)
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
