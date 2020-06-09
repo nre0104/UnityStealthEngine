34c50f257834b0930d2a01f05b3fb44e5502da15
Isi <Isidoros@gmx.de>
2020-06-09 21:03:59 +0200

    Implemented Moving Enemy

--- UnityStealthEngine/Assets/Scripts/EnemyController.cs
+++ UnityStealthEngine/Assets/Scripts/EnemyController.cs
@@ -3,33 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+//https://docs.unity3d.com/Manual/nav-AgentPatrol.html
+
 public class EnemyController : MonoBehaviour
 {
     public float LookRadius = 6f;
     NavMeshAgent agent;
     Transform target;
+    private Vector3 startingPosition;
+    public Transform[] points;
+    private int destPoint = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
+        startingPosition = transform.position;;
         target = GameManager.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        //ChasePlayer();
+        if (!agent.pathPending && agent.remainingDistance < 2f)
+            GotoNextPoint();
+    }
+
+    void ChasePlayer()
     {
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if(distance <= LookRadius)
+        if (distance <= LookRadius)
         {
             agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
         }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, LookRadius); 
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
     }
 }
