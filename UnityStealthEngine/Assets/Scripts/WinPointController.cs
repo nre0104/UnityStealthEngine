e2625e20346e3e422fa6104319ec9e56d5020904
Nicolas Resch <nresch@stud.hs-heilbronn.de>
2020-06-18 18:27:46 +0200

    Implemented Win view

---
+++ UnityStealthEngine/Assets/Scripts/WinPointController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPointController : MonoBehaviour
+{
+    public GameObject winView;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Time.timeScale = 0.0f;
+        winView.SetActive(true);
+    }
+}
