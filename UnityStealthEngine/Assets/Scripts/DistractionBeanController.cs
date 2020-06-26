c33c7350fc54df42354d4be8658ee2a0c1c0bc8c
Nicolas Resch <nresch@stud.hs-heilbronn.de>
2020-06-26 16:47:26 +0200

    Renamed

---
+++ UnityStealthEngine/Assets/Scripts/DistractionBeanController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DistractionBeanController : MonoBehaviour
+{
+    private Rigidbody myBody;
+    public float lifeTimer = 20f;
+    private float timer;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= lifeTimer)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
